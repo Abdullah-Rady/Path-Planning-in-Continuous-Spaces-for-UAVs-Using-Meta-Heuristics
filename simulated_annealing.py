8844fd29f4ad7ba3d1daa91fe6118c76c3b3f36e
Abdullah Rady <abdosam@live.co.uk>
2023-10-23 22:05:17 +0300

    finished simulated annealing

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-from objective_function import calculate_total_fitness,check_feasibility
+from objective_function import calculate_total_fitness, generate_initial_solution
 
 
 numberOfDrones = 3  # Number of drones
@@ -20,63 +20,74 @@ obstacles = [(),(),()] # List of obstacles
 
 
 
-def generateInitialSolution():
-    """
-    Generate an initial solution to the problem.
+# Define your stopping criterion
+def stopping_criterion(iteration):
+    iteration > max_iterations
+    # Implement your stopping criterion logic
+    # Return True if you want to stop, otherwise False
+    pass
 
-    Returns:
-        list: Initial solution.
-    """
-    # Generate a random list of the following form [(x1,y1,z1),(x2,y2,z2),...]
-    # where (xi,yi,zi) is the position of the ith drone
-    while True:
-        initial_solution = []
-        for _ in range(numberOfDrones):
-            drone_points = []
-            for _ in range(numberOfControlPoints):
-                x = random.uniform(0, size_of_field)
-                y = random.uniform(0, size_of_field)
-                z = random.uniform(0, size_of_field)
-                drone_points.append((x, y, z))
-            initial_solution.append(drone_points)
-        if(check_feasibility(starting_points,target_points,initial_solution,obstacles)):
-            break
-    return initial_solution
+def cooling_schedule(T_current):
+    # Define your cooling schedule here (e.g., T_current = 0.9 * T_current)
+    return T_current * 0.9
+    
 
 
-
-
-def simulated_annealing():
-    current_solution = generateInitialSolution  # Initial solution
+def simulated_annealing(initial_temperature, final_temperature):
+    current_solution = generate_initial_solution  # Initial solution is the simplified path
     best_solution = current_solution  # Best solution found so far
-    best_solution_value = calculate_total_fitness(current_solution,starting_points,target_points)  # Objective value of the best solution
-    currentTemperature = initialTemperature 
+    best_solution_value = calculate_total_fitness(current_solution)  # Objective value of the best solution
+
+    current_temperature = initial_temperature
     iteration = 0
 
-    while currentTemperature > finalTemperature and iteration < max_iterations:
+    while current_temperature > final_temperature and not stopping_criterion(iteration):
         for _ in range(n_iterations):
-            fitness_value = calculate_total_fitness(x,starting_points,target_points)
+            fitness_value = calculate_total_fitness(current_solution)
+
             # Generate a new solution x_new
-            x_new = [x_current[i] + random.uniform(-1, 1) for i in range(len(x_current))]
+            # r1 is a random index of a path, and r2 is a random index of a point within the path
+            r1 = random.randint(0, len(current_solution) - 1)
+            path = current_solution[r1]
+            r2 = random.randint(0, len(path) - 1)
+
+            # Generate new x, y, z coordinates for the selected point
+            x_new = list(path[r2])  # Make a copy of the selected point
+
+            # Modify the x, y, z values as needed, e.g., add random perturbation
+            x_new[0] += random.uniform(-1, 1)  # Modify x
+            x_new[1] += random.uniform(-1, 1)  # Modify y
+            x_new[2] += random.uniform(-1, 1)  # Modify z
+
+            # Create a new solution by replacing the selected point in the path
+            new_solution = current_solution[:]
+            new_solution[r1] = path[:r2] + [tuple(x_new)] + path[r2 + 1:]
+               
+
+            # Implement a way to perturb the solution based on your problem's characteristics
+            # You can use the simplified path to guide the perturbation
 
             # Calculate the change in energy (objective value)
-            delta_E = calculate_total_fitness(x_new,starting_points,target_points) - calculate_total_fitness(x_new,starting_points,target_points)
+            delta_E = calculate_total_fitness(new_solution) - fitness_value
 
             if delta_E < 0:
                 # Accept the better solution
-                x_current = x_new
-            elif random.random() < math.exp(-delta_E / T_current):
+                current_solution = new_solution
+            elif random.random() < math.exp(-delta_E / current_temperature):
                 # Accept the solution with a probability
-                x_current = x_new
+                current_solution = new_solution
 
             # Update the best solution if necessary
-            if calculate_total_fitness(x_current,starting_points,target_points) < f_best:
-                x_best = x_current
-                f_best = objective_function(x_current)
+            if calculate_total_fitness(current_solution) < best_solution_value:
+                best_solution = current_solution
+                best_solution_value = calculate_total_fitness(current_solution)
 
         # Update temperature and iteration counter
-        T_current = cooling_schedule(T_current)
+        current_temperature = cooling_schedule(current_temperature)
         iteration += 1
+    # Return the best solution found
+    return best_solution
+
 
 # Output the best solution found
 print("Best Solution:", x_best)
